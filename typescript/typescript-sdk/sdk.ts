6d98fbcecd1bb3254712545b08054e71cab054d7
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-03-12 22:27:32 +0100

    Update thirdweb-sdk to latest

--- typescript/typescript-sdk/sdk.ts
+++ typescript/typescript-sdk/sdk.ts
@@ -1,15 +1,15 @@
 // Importing libraries
-import { ThirdwebSDK } from "@3rdweb/sdk";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { ethers } from "ethers";
 import dotenv from "dotenv";
 dotenv.config();
 
-//Instantiate 3rdweb SDK
+// Instantiate thirdweb SDK
 const sdk = new ThirdwebSDK(
   new ethers.Wallet(
     // Your wallet private key
     process.env.PRIVATE_KEY as string,
     // RPC URL, we'll use Polygon Mumbai
-    ethers.getDefaultProvider("https://rpc-mumbai.maticvigil.com")
+    ethers.getDefaultProvider("https://matic-mainnet.chainstacklabs.com")
   )
 );

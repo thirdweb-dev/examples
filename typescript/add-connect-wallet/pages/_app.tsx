8c0e0855318a9b381b1211438df4be3d17d713ac
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-03-14 11:35:13 +0100

    Setup _app.tsx

---
+++ typescript/add-connect-wallet/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThirdwebProvider } from '@thirdweb-dev/react';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const desiredChainId = 80001;
+
+  /**
+   * Make sure that your app is wrapped with these contexts.
+   * If you're using React, you'll have to replace the Component setup with {children}
+   */
+  return (
+    <ThirdwebProvider desiredChainId={desiredChainId}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;

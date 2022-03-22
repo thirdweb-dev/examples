1c779bf5dad3f0e48e10cf99c72a0ae9824bd9bf
ayush <62694274+ayshptk@users.noreply.github.com>
2022-03-23 05:13:24 +0530

    change ipfs url

--- typescript/poap/utils/parseIpfs.js
+++ typescript/poap/utils/parseIpfs.js
@@ -1,6 +1,6 @@
 export default function parseUrl(url) {
   if (url.includes("ipfs://")) {
-    return url.replace("ipfs://", "https://ipfs.thirdweb.com/ipfs/");
+    return url.replace("ipfs://", "https://gateway.ipfscdn.io/ipfs/");
   } else {
     return url;
   }

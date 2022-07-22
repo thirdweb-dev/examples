568ab5747cf8298433c93caaad563197dee652be
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-07-22 17:22:38 +0530

    fix: improve security of set claimed route

--- typescript/free-first-mint/pages/api/set-claimed.ts
+++ typescript/free-first-mint/pages/api/set-claimed.ts
@@ -8,39 +8,44 @@ const generateMintSignature = async (
 ) => {
   const { address } = JSON.parse(req.body);
 
-  //   const drop = sdk.getSignatureDrop(
-  //     "0x6d148a12f7c0ae693609F5a26E085646f8F73A53"
-  //   );
-  //   const nfts = await drop.getOwned(address);
+  const drop = sdk.getSignatureDrop(
+    "0x6d148a12f7c0ae693609F5a26E085646f8F73A53"
+  );
+  const balance = await drop.balanceOf(address);
+  console.log(balance.toNumber());
 
-  //   if (nfts.length > 0) {
-  const record = await table
-    .select({
-      fields: ["address", "hasClaimed"],
-      filterByFormula: `NOT({address} != '${address}')`,
-    })
-    .all();
+  if (balance.toNumber() > 0) {
+    const record = await table
+      .select({
+        fields: ["address", "hasClaimed"],
+        filterByFormula: `NOT({address} != '${address}')`,
+      })
+      .all();
 
-  try {
-    if (record.length > 0) {
-      record[0].updateFields({
-        hasClaimed: "true",
+    try {
+      if (record.length > 0) {
+        record[0].updateFields({
+          hasClaimed: "true",
+        });
+      } else {
+        await table.create({
+          address: address,
+          hasClaimed: "true",
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
       });
-    } else {
-      await table.create({
-        address: address,
-        hasClaimed: "true",
+    } catch (err) {
+      res.status(500).json({
+        error: err,
       });
     }
-
-    return res.status(200).json({
-      success: true,
-    });
-  } catch (err) {
-    res.status(500).json({
-      error: err,
-    });
   }
+  res.status(400).json({
+    error: "The user hasn't claimed NFT yet.",
+  });
 };
 
 export default generateMintSignature;

1a2aa6a7eeea3454088c3a501dd9b1f678b73032
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-07-29 09:04:39 +0530

    refactor

--- typescript/on-demand-pass/pages/api/generate-mint-sig.ts
+++ typescript/on-demand-pass/pages/api/generate-mint-sig.ts
@@ -13,23 +13,25 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     "0xeED8165505d78D2CA9f2b4fA6Aff179CeBd4dCA4"
   );
 
+  const metadata = {
+    name: "Test title",
+    description: "Test description",
+    image:
+      "https://images.unsplash.com/photo-1658309833607-4de9956d0bbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+    properties: {
+      type: "test",
+    },
+  };
+
   try {
     const signedPayload = await collection.signature.generate({
       to: address,
-      metadata: {
-        name: "Test title",
-        description: "Test description",
-        image:
-          "https://images.unsplash.com/photo-1658309833607-4de9956d0bbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
-        properties: {
-          type: "test",
-        },
-      },
+      metadata,
       price: "1",
     });
 
     return res.status(200).json({
-      signedPayload: signedPayload,
+      signedPayload,
     });
   } catch (error) {
     console.log(error);

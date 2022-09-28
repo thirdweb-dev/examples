7a745969562981f06284fb29e04a582bb70e16e9
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-09-28 23:07:18 +0530

    refactor: clean up code (#88)

--- typescript/multi-chain-dapp/pages/_app.tsx
+++ typescript/multi-chain-dapp/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { ChainId } from "@thirdweb-dev/sdk";
 import type { AppProps } from "next/app";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import ChainContext from "../context/Chain";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [selectedChain, setSelectedChain] = useState(ChainId.Mumbai);
 
   return (
     <ChainContext.Provider value={{ selectedChain, setSelectedChain }}>
-      <Component {...pageProps} />
+      <ThirdwebProvider desiredChainId={selectedChain}>
+        <Component {...pageProps} />
+      </ThirdwebProvider>
     </ChainContext.Provider>
   );
 }
--- typescript/multi-chain-dapp/pages/index.tsx
+++ typescript/multi-chain-dapp/pages/index.tsx
@@ -1,17 +1,36 @@
 import type { NextPage } from "next";
 
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import HomeComponent from "../components/Home";
-import { useContext } from "react";
+import { ChainId, Web3Button } from "@thirdweb-dev/react";
+import { FC, useContext } from "react";
 import ChainContext from "../context/Chain";
 
 const Home: NextPage = () => {
-  const { selectedChain } = useContext(ChainContext);
+  const { selectedChain, setSelectedChain } = useContext(ChainContext);
+
+  const addresses = {
+    [String(ChainId.Mumbai)]: "0x25CB5C350bD3062bEaE7458805Fb069200e37fD5",
+    [String(ChainId.Goerli)]: "0xA72234a2b9c1601593062f333D739C93291dF49F",
+  };
 
   return (
-    <ThirdwebProvider desiredChainId={selectedChain}>
-      <HomeComponent />
-    </ThirdwebProvider>
+    <div>
+      <select
+        value={String(selectedChain)}
+        onChange={(e) => setSelectedChain(parseInt(e.target.value))}
+      >
+        <option value={String(ChainId.Mumbai)}>Mumbai</option>
+        <option value={String(ChainId.Goerli)}>Goerli</option>
+      </select>
+
+      <div style={{ maxWidth: "200px" }}>
+        <Web3Button
+          contractAddress={addresses[String(selectedChain)]}
+          action={(contract) => contract.erc721.claim(1)}
+        >
+          Claim
+        </Web3Button>
+      </div>
+    </div>
   );
 };
 

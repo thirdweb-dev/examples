80da0001d7d56b746bf8d47450a1cd36a3a825e2
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-11-15 22:13:04 +0530

    update some stuff

--- javascript/gasless-nft-drop/pages/index.js
+++ javascript/gasless-nft-drop/pages/index.js
@@ -1,28 +1,38 @@
 import { ConnectWallet, useAddress, useContract } from "@thirdweb-dev/react";
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const address = useAddress();
   const { contract } = useContract(
-    "0xcDFA0Db479d35e1D1C76bf2D65166e32Ce42F048"
+    "0xcDFA0Db479d35e1D1C76bf2D65166e32Ce42F048",
+    "edition-drop"
   );
+  const [loading, setLoading] = useState(false);
 
   const tokenId = 0;
   const quantity = 1;
 
   const claimNFT = async () => {
-    try {
-      await contract?.claimTo(address, tokenId, quantity);
-      console.log("🎉 NFT claimed successfully!");
-    } catch (err) {
-      console.log("💩 Error claiming NFT: ", err);
+    if (address && contract) {
+      setLoading(true);
+      try {
+        await contract.erc1155?.claimTo(address, tokenId, quantity);
+        console.log("🎉 NFT claimed successfully!");
+      } catch (err) {
+        console.log("💩 Error claiming NFT: ", err);
+      } finally {
+        setLoading(false);
+      }
     }
   };
 
   return (
     <div className={styles.container}>
       {address ? (
-        <button onClick={claimNFT}>Claim NFT</button>
+        <button onClick={claimNFT}>
+          {loading ? "Claiming..." : "Claim NFT"}
+        </button>
       ) : (
         <ConnectWallet />
       )}

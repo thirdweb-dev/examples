74efb841b88c40225c5faef4af5c1ad90bef5433
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-11-15 22:01:23 +0530

    update to v3

--- javascript/gasless-nft-drop/pages/index.js
+++ javascript/gasless-nft-drop/pages/index.js
@@ -1,19 +1,24 @@
-import { useMetamask, useAddress, useEditionDrop } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  useAddress,
+  useContract,
+  useMetamask,
+} from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const connectWithMetamask = useMetamask();
   const address = useAddress();
-  const editionDrop = useEditionDrop(
+  const { contract } = useContract(
     "0xcDFA0Db479d35e1D1C76bf2D65166e32Ce42F048"
   );
-  
+
   const tokenId = 0;
   const quantity = 1;
 
   const claimNFT = async () => {
     try {
-      await editionDrop?.claimTo(address, tokenId, quantity);
+      await contract?.claimTo(address, tokenId, quantity);
       console.log("🎉 NFT claimed successfully!");
     } catch (err) {
       console.log("💩 Error claiming NFT: ", err);
@@ -25,7 +30,7 @@ export default function Home() {
       {address ? (
         <button onClick={claimNFT}>Claim NFT</button>
       ) : (
-        <button onClick={connectWithMetamask}>Sign in with metamask</button>
+        <ConnectWallet />
       )}
     </div>
   );

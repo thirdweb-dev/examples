60b9e62479086eab1a79380eb1166dca1a445262
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-11-15 22:22:31 +0530

    cleanup

--- javascript/gasless-nft-drop/pages/index.js
+++ javascript/gasless-nft-drop/pages/index.js
@@ -1,41 +1,22 @@
-import { ConnectWallet, useAddress, useContract } from "@thirdweb-dev/react";
-import { useState } from "react";
+import { Web3Button } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const address = useAddress();
-  const { contract } = useContract(
-    "0xcDFA0Db479d35e1D1C76bf2D65166e32Ce42F048",
-    "edition-drop"
-  );
-  const [loading, setLoading] = useState(false);
-
   const tokenId = 0;
   const quantity = 1;
 
-  const claimNFT = async () => {
-    if (address && contract) {
-      setLoading(true);
-      try {
-        await contract.erc1155?.claimTo(address, tokenId, quantity);
-        console.log("🎉 NFT claimed successfully!");
-      } catch (err) {
-        console.log("💩 Error claiming NFT: ", err);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
-
   return (
     <div className={styles.container}>
-      {address ? (
-        <button onClick={claimNFT}>
-          {loading ? "Claiming..." : "Claim NFT"}
-        </button>
-      ) : (
-        <ConnectWallet />
-      )}
+      <Web3Button
+        contractAddress="0xcDFA0Db479d35e1D1C76bf2D65166e32Ce42F048"
+        action={(contract) =>
+          contract.erc1155.claimTo(address, tokenId, quantity)
+        }
+        onSuccess={() => alert("Claimed!")}
+        onError={() => alert("Something went wrong")}
+      >
+        Claim
+      </Web3Button>
     </div>
   );
 }

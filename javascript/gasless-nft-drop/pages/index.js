8c1f118e2c229ba2287bfa5992fabc57a2330545
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-11-16 00:28:47 +0530

    update to v3 (#90)
    * update to v3
    
    * remove unused stuff
    
    * update some stuff
    
    * cleanup
    
    * plz

--- javascript/gasless-nft-drop/pages/index.js
+++ javascript/gasless-nft-drop/pages/index.js
@@ -1,32 +1,20 @@
-import { useMetamask, useAddress, useEditionDrop } from "@thirdweb-dev/react";
+import { Web3Button } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const connectWithMetamask = useMetamask();
-  const address = useAddress();
-  const editionDrop = useEditionDrop(
-    "0xcDFA0Db479d35e1D1C76bf2D65166e32Ce42F048"
-  );
-  
   const tokenId = 0;
   const quantity = 1;
 
-  const claimNFT = async () => {
-    try {
-      await editionDrop?.claimTo(address, tokenId, quantity);
-      console.log("🎉 NFT claimed successfully!");
-    } catch (err) {
-      console.log("💩 Error claiming NFT: ", err);
-    }
-  };
-
   return (
     <div className={styles.container}>
-      {address ? (
-        <button onClick={claimNFT}>Claim NFT</button>
-      ) : (
-        <button onClick={connectWithMetamask}>Sign in with metamask</button>
-      )}
+      <Web3Button
+        contractAddress="0xcDFA0Db479d35e1D1C76bf2D65166e32Ce42F048"
+        action={(contract) => contract.erc1155.claim(tokenId, quantity)}
+        onSuccess={() => alert("Claimed!")}
+        onError={() => alert("Something went wrong")}
+      >
+        Claim
+      </Web3Button>
     </div>
   );
 }
